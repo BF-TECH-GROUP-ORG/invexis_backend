a84e8aa37a9c6d73e145cb5ef983292e82094224
frankbahirwa <frankbahirwa@gmail.com>
2025-09-06 18:15:42 +0200

    folder structure

--- services/shop-service/src/index.js
+++ services/shop-service/src/index.js
@@ -1,16 +1,9 @@
 require("dotenv").config();
 const express = require("express");
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-const shopRoutes = require("./routes/shopRoutes");
-
 const app = express();
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use("/api/shops", shopRoutes);
-
+app.get("/", (req, res) => {
+  res.send("Shop Service is running");
+});
 const PORT = process.env.PORT || 4009;
 app.listen(PORT, () => {
   console.log(`Shop Service running on port ${PORT}`);
