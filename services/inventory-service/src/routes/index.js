d9b7229ca39cea96c6b2484f5086f93175ed1451
germain250 <irakozemugishagermain@gmail.com>
2025-10-22 11:42:22 +0200

    added forgotten routes and fixed port issue

--- services/inventory-service/src/routes/index.js
+++ services/inventory-service/src/routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const productRoutes = require('./productRoutes');
-const stockChangeRoutes = require('./inventoryAdjustmentRoutes');
+const stockChangeRoutes = require('./stockChangeRoutes');
+const inventoryAdjustmentRoutes = require('./inventoryAdjustmentRoutes');
+const reportRoutes = require('./reportRoutes');
 //const favoriteRoutes = require('./favoriteRoute');
 const discountRoutes = require('./discountRoutes');
 const alertRoutes = require('./alertRoutes');
@@ -13,9 +15,10 @@ router.get('/', (req, res) => {
 
 router.use('/v1/products', productRoutes);
 router.use('/v1/stock-changes', stockChangeRoutes);
-//router.use('/v1/favorites', favoriteRoutes);
 router.use('/v1/discounts', discountRoutes);
 router.use('/v1/alerts', alertRoutes);
 router.use('/v1/categories', categoryRoutes);
+router.use('/v1/report', reportRoutes)
+router.use('v1/inventory-adjustment', inventoryAdjustmentRoutes)
 
 module.exports = router;
